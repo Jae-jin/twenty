d527f3d66d3625c1f8f56e6d94fc9c7a4192e404
EC2 Default User <ec2-user@ip-172-31-42-179.us-east-2.compute.internal>
2018-05-22 12:08:46 +0000

    I add my function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  
+  
+  resources :users
   resources :posts do
     resources :comments
   end
   
-  root 'posts#index'
+  get 'login' => 'sessions#new'
+  post 'login' => 'posts#index'
+  delete 'logout' => 'sessions#destroy'
+  
+  root "users#index"
+  # root 'posts#index'
   post 'posts/comment_create/:post_id' => 'posts#comment_create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_064432) do
+ActiveRecord::Schema.define(version: 2018_05_21_144329) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2018_05_15_064432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
